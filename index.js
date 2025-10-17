cbb47ba569711ae4a0056eb1690fc5a3956b6899
James Gill <james@roboticmonsters.com>
2025-10-17 14:01:45 +1000

    test-release: add typescript parser, json parser, github release workflows

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
     './config/base.js',
     './config/imports.js',
     './config/typescript.js',
+    './config/json.js',
     './config/view.js',
     './config/e2e.js',
     './config/testing.js',
